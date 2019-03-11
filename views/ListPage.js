611396e787f6d5c7f8c1b30fd63a42ce928d4199
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-11 19:58:15 +0100

    Update ListPage.js

--- views/ListPage.js
+++ views/ListPage.js
@@ -102,8 +102,8 @@ class ListPageScreen extends React.Component {
           key={Math.random()}
           style={styles.item}
         >
-        <ImageBackground source={{uri: item.uri}} style={styles.item, {width: '100%', height: '100%', opacity: 0.2}}>
-          <Text style={{flex:1, textAlign: 'center', textAlignVertical: 'center',color: item.color, fontSize: item.fontSize}}>{item.value}</Text>
+        <ImageBackground source={{uri: item.uri}} style={styles.item, {width: '100%', height: '100%', opacity: 0.4}}>
+          <Text style={{flex:1, textAlign: 'center', textAlignVertical: 'center',color: item.color, fontSize: item.fontSize, , opacity: 1}}>{item.value}</Text>
         </ImageBackground>
     
         </TouchableOpacity>
