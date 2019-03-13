2d010cdb268790f3741cb9870c1748f8648970bf
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-13 17:12:27 +0100

    Update ListPage.js

--- views/ListPage.js
+++ views/ListPage.js
@@ -129,7 +129,7 @@ class ListPageScreen extends React.Component {
         >
           <ImageBackground 
             source={{uri: item.uri}} 
-            style={{flex:1,height:Dimensions.get('window').width/numColumns,width:'100%',height:'100%',position:'absolute'},
+            style={{flex:1,width:'100%',height:'100%',position:'absolute'},
             styleImage
           }/>
           <Text style={{flex:1, textAlign: 'center', textAlignVertical: 'center', position: 'absolute'},styleText}>{item.value}</Text>
