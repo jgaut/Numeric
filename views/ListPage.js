ad056bebf9eabe84d6e44b275b3ef397e90b3af4
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-15 14:01:26 +0100

    Update ListPage.js

--- views/ListPage.js
+++ views/ListPage.js
@@ -87,12 +87,12 @@ class ListPageScreen extends React.Component {
                         tmp2.uri=response.url;
                         tmp2.key=item.key;
                         tmp2.lastModified=item.lastModified;
-                        this.state.data.list.push(tmp2);
+                        tmp.push(tmp2);
                         console.log(tmp2);
                         //console.log(data.key);
                         cpt++;
                         if(cpt==taille){
-                          //this.state.data.list=tmp;
+                          this.state.data.list=tmp;
                           this.forceUpdate();
                           console.log("this.state.data.list :"+JSON.stringify(this.state.data.list));
                           console.log('Update view !');
@@ -114,7 +114,7 @@ class ListPageScreen extends React.Component {
 
     return (
       <FlatList
-        data={formatData(this.state.data.list, numColumns)}
+        data={()=>{formatData(this.state.data.list, numColumns);}}
         style={styles.container}
         renderItem={this.renderItem}
         numColumns={numColumns}
