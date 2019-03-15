f13543466899e1ba931cfe58ccf72c92130efd34
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-15 16:46:39 +0100

    Update ListPage.js

--- views/ListPage.js
+++ views/ListPage.js
@@ -81,11 +81,11 @@ class ListPageScreen extends React.Component {
           //console.log("ancien élément : " + JSON.stringify(oldElement));
 
           //Si l'indicateur a été mis à jour.
-          /*if(!maj){
+          if(!maj){
             tmp.push(this.state.list[indice]);
           }else{
             console.log('MAJ element : '+item.key);
-*/
+
           //Récupération de l'élément
           Storage.get(item.key, {level: 'public'})
             .then(result => {
@@ -143,6 +143,7 @@ class ListPageScreen extends React.Component {
                   }).catch(error => {console.log(error);});
             }).catch(err => console.log(err));
         }
+      }
       });
     
     });
