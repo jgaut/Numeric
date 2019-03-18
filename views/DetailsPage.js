ea67e42fc381120d88bae5a74895a3b8f10b39d2
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-18 12:10:56 +0100

    Update DetailsPage.js

--- views/DetailsPage.js
+++ views/DetailsPage.js
@@ -14,13 +14,41 @@ class DetailsPageScreen extends React.Component {
     
     const {navigate} = this.props.navigation;
 
+    const data = [ 50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80 ]
+
+        const axesSvg = { fontSize: 10, fill: 'grey' };
+        const verticalContentInset = { top: 10, bottom: 10 }
+        const xAxisHeight = 30
+
     return (
 
-      <View style={styles.container}>
-        <Text>{this.props.navigation.state.params.key}
-        </Text>
-        
-      </View>
+return (
+            <View style={{ height: 200, padding: 20, flexDirection: 'row' }}>
+                <YAxis
+                    data={data}
+                    style={{ marginBottom: xAxisHeight }}
+                    contentInset={verticalContentInset}
+                    svg={axesSvg}
+                />
+                <View style={{ flex: 1, marginLeft: 10 }}>
+                    <LineChart
+                        style={{ flex: 1 }}
+                        data={data}
+                        contentInset={verticalContentInset}
+                        svg={{ stroke: 'rgb(134, 65, 244)' }}
+                    >
+                        <Grid/>
+                    </LineChart>
+                    <XAxis
+                        style={{ marginHorizontal: -10, height: xAxisHeight }}
+                        data={data}
+                        formatLabel={(value, index) => index}
+                        contentInset={{ left: 10, right: 10 }}
+                        svg={axesSvg}
+                    />
+                </View>
+            </View>
+        )
 
         
     );
