98ea665bb2ed60b118eeaac40210c1373af6863c
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-18 15:48:33 +0100

    Update DetailsPage.js

--- views/DetailsPage.js
+++ views/DetailsPage.js
@@ -49,7 +49,7 @@ class DetailsPageScreen extends React.Component {
     const xAxisHeight = 30
 
     return (
-            <View style={{ height: 200, padding: 20, flexDirection: 'row' }}>
+            <View style={{ height: 200, padding: 20, flexDirection: 'column' }}>
                 <YAxis
                     data={this.state.dataY}
                     style={{ marginBottom: xAxisHeight }}
