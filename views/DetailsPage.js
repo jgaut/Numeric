f970661641410d39fc99ffd1c8e9e59d246f23b6
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-22 14:32:06 +0100

    Update DetailsPage.js

--- views/DetailsPage.js
+++ views/DetailsPage.js
@@ -46,7 +46,7 @@ class DetailsPageScreen extends React.Component {
                 .then(result => {
                     fetch(result).then(response => response.json()).then(data => {
                         //console.log(data);
-                        console.log('X : ' + JSON.stringify(this.state.data[g]));
+                        console.log('X : ' + JSON.stringify(this.state));
                         this.state.data[g]["X"] = [];
                         this.state.data[g]["Y"] = [];
 
