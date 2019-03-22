969aabedce3b8da349f6b0d91731fed35a4dfd26
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-22 18:11:57 +0100

    Update DetailsPage.js

--- views/DetailsPage.js
+++ views/DetailsPage.js
@@ -51,7 +51,8 @@ class DetailsPageScreen extends React.Component {
                         data.forEach(item=>{
                             this.state["data"][0]["X"].push(parseFloat(item['_time']));
                             this.state["data"][0]["Y"].push(parseFloat(item['value']));
-                            console.log(strftimeCEST('%F %T', new Date(parseFloat(item['_time']))));
+                            console.log(new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(parseFloat(item['_time'])));
+                            
                             //console.log(JSON.stringify(dataTmp));
                         });
 
