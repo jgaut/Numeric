1b1f042a5723d7dec8a4115d7ecb6e31317a7240
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-23 13:14:20 +0100

    Update DetailsPage.js

--- views/DetailsPage.js
+++ views/DetailsPage.js
@@ -128,7 +128,7 @@ class DetailsPageScreen extends React.Component {
                         formatLabel={(value, index) => {return this.state.data[0]["X"][index];}}
                         contentInset={{ left: 10, right: 10 }}
                         svg={axesSvg}
-                        xScale={d3Scale.scaleTime}
+                        xScale={d3scale.scaleTime}
                     />
                 </View>
             </View>
