abb52d83c3bcce3a46f5fe30b6289dad8dbd17f5
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-24 19:49:49 +0100

    Update DetailsPage.js

--- views/DetailsPage.js
+++ views/DetailsPage.js
@@ -12,7 +12,7 @@ class DetailsPageScreen extends React.Component {
   constructor(...args) {
     super(...args);
 
-    this.state = {dataX:[], dataY:[], key:''};
+    this.state = {dataX:['0', '1'], dataY:[], key:''};
 
     this.props.navigation.addListener('didFocus', () => {
         this.loadDetails();
