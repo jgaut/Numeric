913bf5ade58acdcce8b77321c8c0e7f1712c0aaa
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-24 21:04:47 +0100

    Update DetailsPage.js

--- views/DetailsPage.js
+++ views/DetailsPage.js
@@ -118,7 +118,7 @@ class DetailsPageScreen extends React.Component {
                     numberOfTicks={ 6 }
                     style={{ marginHorizontal: -15, height: 20 }}
                     contentInset={{ left: 10, right: 25 }}
-                    formatLabel={ (value) => parseFloat(value)}
+                    formatLabel={ (value) => value}
                 />
             </View>
         );
