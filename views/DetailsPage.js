0f8ad1b1aac92b6a52d8320a5bc0af464002e4b9
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-24 20:42:33 +0100

    Update DetailsPage.js

--- views/DetailsPage.js
+++ views/DetailsPage.js
@@ -85,8 +85,8 @@ class DetailsPageScreen extends React.Component {
 
                             //this.state.dataX.push(Moment.unix(parseFloat(item['_time'])).format("D"));
                             //this.state.dataY.push(parseFloat(item['value']));
-                            var myDate = new Date(item['_time']);
-                            this.state.data.push({date: myDate, value: item['value']};
+                            var myDate = item['_time'];
+                            this.state.data.push({date: new Date(myDate), value: item['value']};
                             //console.log(Moment.unix(parseFloat(item['_time'])).format("MM/DD/YYYY")); //basically you can do all sorts of the formatting and others
 
                             
