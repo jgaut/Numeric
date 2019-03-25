2a555dd2625631feecdef838eea942cb0236c3c3
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-25 17:41:38 +0100

    Update DetailsPage.js

--- views/DetailsPage.js
+++ views/DetailsPage.js
@@ -87,11 +87,11 @@ class DetailsPageScreen extends React.Component {
   render() {
     
     //const {navigate} = this.props.navigation;
-    //console.log('X : ' + this.state.data[0]["X"]);
+    console.log('X : ' + JSON.stringify(this.state.dataX));
     const axesSvg = { fontSize: 8, fill: 'grey' };
     const verticalContentInset = {};
     const xAxisHeight = 10;
-  
+    
     return (
         <View style={{ height: 200, padding: 20, flexDirection: 'row' }}>
                 <YAxis
