715a38476a3b8eef9f5b1a2a8d642587542256f0
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-26 16:01:37 +0100

    Update DetailsPage.js

--- views/DetailsPage.js
+++ views/DetailsPage.js
@@ -28,7 +28,7 @@ class DetailsPageScreen extends React.Component {
 
     loadDetails(){
 
-    for(int i=0; i<3;i++){
+    for(var i=0; i<3;i++){
 
     var regex = /numeric_/gi;
     var tmp = this.props.navigation.state.params.key;
