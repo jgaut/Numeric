e0b332677d44a1119dd5ea52217894abf9ac0df0
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-03-26 16:45:36 +0100

    Update DetailsPage.js

--- views/DetailsPage.js
+++ views/DetailsPage.js
@@ -64,7 +64,7 @@ class DetailsPageScreen extends React.Component {
     console.log(this.state.dataX.length);
     console.log(this.state.dataY.length);
 
-    if(this.state.dataX.length <= 0){
+    if(this.state.dataX.length <= 0 || false){
         console.log("x=>"+this.state.dataX.length);
         return (<View></View>);
     }else{
