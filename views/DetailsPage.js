63f5f4afd5f3ca0302930b6b3375ab6a047c3114
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-04-25 20:46:12 +0200

    Update DetailsPage.js

--- views/DetailsPage.js
+++ views/DetailsPage.js
@@ -48,7 +48,7 @@ class DetailsPageScreen extends React.Component {
                         });
                         this.state.dataX[i] = [...new Set(this.state.dataX[i])]; 
                         
-                        this.forceUpdate();
+                        //this.forceUpdate();
                     });
                 });  
         }
