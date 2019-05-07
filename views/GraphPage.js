5316f558a3a2303ef76718275828cdd56b010c57
Jérémy GAUTIER <gautierjeremy@gmail.com>
2019-05-07 14:29:45 +0200

    Create GraphPage.js

---
+++ views/GraphPage.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions} from 'react-native';
+import { Constants } from 'expo';
+import { Grid, LineChart, XAxis, YAxis, AreaChart } from 'react-native-svg-charts'
+import Storage from '@aws-amplify/storage';
+import { createStackNavigator } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import Moment from 'moment';
+import * as scale from 'd3-scale'
+import * as shape from 'd3-shape'
+import dateFns from 'date-fns'
+
+class GraphPageScreen extends React.Component {
+  
+    constructor(...args) {
+        super(...args);
+
+        this.state = {};
+
+        this.props.navigation.addListener('didBlur', () => {
+        });
+
+    }
+
+    componentDidMount(){
+        this.props.navigation.addListener('didFocus', () => {
+            this.loadDetails();
+        });
+    }
+
+    
+
+    render() {
+
+
+        return (
+            <View>
+             
+
+            </View>
+        );
+
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight
+  },
+});
+
+export default GraphPageScreen;
